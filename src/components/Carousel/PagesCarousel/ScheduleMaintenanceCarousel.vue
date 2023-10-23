cdd8f0857505dfc1dd1114d92a198a8403b0eb83
Lucas José <lukinhas8979@gmail.com>
2023-10-23 01:06:22 -0300

    lukaian@01:04-23/10/23: criando carrosel desktop. 1-falta criar o design mobile + controller. 2-falta melhorar o componente de controller para ser reutilizavel.

---
+++ src/components/Carousel/PagesCarousel/ScheduleMaintenanceCarousel.vue
@@ -0,0 +1,73 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="left-side">
+      <h1>
+        Agende sua Manutenção de
+        <span style="color: var(--primary)">Bicicleta</span>
+      </h1>
+      <p>Agende um horário conveniente para serviços de alta qualidade.</p>
+
+      <button>Agendar Agora</button>
+    </div>
+
+    <div class="right-side">
+      <img src="@/assets/Illustration-maintenance.svg" alt="Imagem" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+}
+
+.left-side {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: flex-start;
+  gap: 20px;
+}
+
+.right-side {
+  flex: 1;
+  text-align: right;
+}
+
+h1,
+span {
+  color: var(--neutral-d-grey);
+  font-size: 64px;
+  font-weight: 600;
+  line-height: 76px;
+}
+
+p {
+  font-size: 16px;
+  color: var(--grey);
+  font-weight: 400;
+  line-height: 24px;
+}
+
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: var(--primary);
+  color: var(--white);
+  font-size: medium;
+
+  margin-top: 10px;
+  padding: 14px 32px;
+  gap: 10px;
+
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <script setup>
 import HeaderController from '@/components/Header/ControllerHeader.vue'
+import CarouselHighlights from '@/components/Carousel/CarouselHighlights.vue'
 </script>
 
 <template>
   <header>
     <HeaderController />
   </header>
+  <main>
+    <CarouselHighlights />
+  </main>
 </template>
 
 <style scoped>
-header {
+header,
+main {
   background-color: var(--color-background-secondary);
   padding: 0 var(--section-gap);
 }
